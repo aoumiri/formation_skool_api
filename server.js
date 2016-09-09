a075bf5855f7fab01449634e929054e1875ae530
Anass Oumiri <aoumiri@octo.com>
2016-09-09 15:05:57 +0200

    feat: Add HAPI root route

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const http = require("http");
+const Hapi = require("hapi");
 
-const server = http.createServer((req, res) => {
-  res.end("Hello world");
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello world!');
+  }
 });
 
-server.listen(8080);
+server.start((err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
