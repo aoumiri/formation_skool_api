5dbf4e9c399a1f448b8615be0c9f9311ce74739c
Anass Oumiri <aoumiri@octo.com>
2016-09-14 14:24:38 +0200

    feat: add swagger

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const Hapi = require('hapi');
+const swagger = require('hapi-swagger');
+const inert = require('inert');
+const vision = require('vision');
 const routes = require('./lib/routes');
 const server = new Hapi.Server();
 
 server.connection({ port: 3000 });
 
 server.route(routes);
+
+server.register([inert, vision, swagger]);
 
 server.start((err) => {
 
