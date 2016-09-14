36842c916b2e681bb029ebe911a9c0bf95c7f30f
Anass Oumiri <aoumiri@octo.com>
2016-09-14 16:31:46 +0200

    fix(swagger): support api_key

--- server.js
+++ server.js
@@ -6,11 +6,31 @@ const routes = require('./lib/routes');
 const server = new Hapi.Server();
 const config = require('./config');
 
-server.connection({ port: config.port });
+server.connection({ port : config.port });
 
 server.route(routes);
 
-server.register([inert, vision, swagger]);
+server.register([
+  inert,
+  vision,
+  {
+    register: swagger,
+    options: {
+      swaggerUIPath: '/documentation/swaggerui/',
+      jsonPath: '/documentation/swagger.json'
+    }
+  }
+]);
+
+server.ext('onRequest', (request, reply) => {
+  if (request.path.startsWith('/documentation')) {
+    return reply.continue();
+  }
+  if (require('./api_keys').indexOf(request.query.api_key) === -1) {
+    return reply().code(401);
+  }
+  return reply.continue();
+});
 
 server.start((err) => {
 
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -5,42 +5,78 @@ chai.config.truncateThreshold = 0;
 
 describe('Server', () => {
   describe('GET /', () => {
-    it('should return the appropriate string', (done) => {
-      server.inject('/', (res) => {
-        expect(res.result).to.equal('Hello world!');
-        done();
+    describe('When api_key is missing', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject('/', (res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
+      });
+    });
+    describe('When api_key is missing', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject('/?query=bad_key', (res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
+      });
+    });
+    describe('When the api_key is correct', () => {
+      it('should return the appropriate string', (done) => {
+        server.inject('/?api_key=anass', (res) => {
+          expect(res.result).to.equal('Hello world!');
+          done();
+        });
       });
     });
   });
   describe('GET /pokemons', () => {
-    it('should return a 200', (done) => {
-      server.inject('/pokemons', (res) => {
-        expect(res.statusCode).to.equal(200);
-        done();
+    describe('When api_key is missing', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject('/pokemons', (res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
       });
     });
-
-    it('should return an array', (done) => {
-      server.inject('/pokemons', (res) => {
-        expect(res.result).to.be.an('array');
-        done();
+    describe('When api_key is incorrect', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject('/pokemons/?query=bad_key', (res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
       });
     });
+    describe('When the correct api_key is given', () => {
+      it('should return a 200', (done) => {
+        server.inject('/pokemons?api_key=anass', (res) => {
+          expect(res.statusCode).to.equal(200);
+          done();
+        });
+      });
 
-    it('should return a list of pokemons', (done) => {
-      server.inject('/pokemons', (res) => {
-        expect(res.result).to.deep.equal(require('./../data'));
-        done();
+      it('should return an array', (done) => {
+        server.inject('/pokemons?api_key=anass', (res) => {
+          expect(res.result).to.be.an('array');
+          done();
+        });
+      });
+
+      it('should return a list of pokemons', (done) => {
+        server.inject('/pokemons?api_key=anass', (res) => {
+          expect(res.result).to.deep.equal(require('./../data'));
+          done();
+        });
       });
     });
 
     describe('when I provide a list of fields', () => {
       it('should return the appropriate fields', (done) => {
-        server.inject('/pokemons?fields=name', (res) => {
+        server.inject('/pokemons?api_key=anass&fields=name', (res) => {
           const pokemons = require('./../data');
           expect(res.result).to.eql(pokemons.map((pokemon) => {
             return {
-              name: pokemon.name
+              name : pokemon.name
             };
           }));
           done();
@@ -50,35 +86,53 @@ describe('Server', () => {
   });
 
   describe('POST /pokemons', () => {
-    describe('when there is no payload', () => {
-      it('should return 400', (done) => {
-        server.inject({ method: 'post', url: '/pokemons' }, (res) => {
-          expect(res.statusCode).to.equal(400);
+    describe('When api_key is missing', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject({ method : 'post', url : '/pokemons' }, (res) => {
+          expect(res.statusCode).to.equal(401);
           done();
         });
       });
     });
-    describe('when there is a payload', () => {
-      describe('when name is missing', () => {
+    describe('When api_key is incorrect', () => {
+      it('should reject the request with a 401 status code', (done) => {
+        server.inject({ method : 'post', url : '/pokemons?api_key=bad_key' }, (res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
+      });
+    });
+    describe('When the correct api_key is given', () => {
+      describe('when there is no payload', () => {
         it('should return 400', (done) => {
-          server.inject({ method: 'post', url: '/pokemons', payload: {} }, (res) => {
+          server.inject({ method : 'post', url : '/pokemons?api_key=anass' }, (res) => {
             expect(res.statusCode).to.equal(400);
             done();
           });
         });
       });
-      it('should return 201', (done) => {
-        server.inject({ method: 'post', url: '/pokemons', payload: { name: 'Foobar' } }, (res) => {
-          expect(res.statusCode).to.equal(201);
-          done();
+      describe('when there is a payload', () => {
+        describe('when name is missing', () => {
+          it('should return 400', (done) => {
+            server.inject({ method : 'post', url : '/pokemons?api_key=anass', payload : {} }, (res) => {
+              expect(res.statusCode).to.equal(400);
+              done();
+            });
+          });
         });
-      });
-      it('should add a new pokemon to the list', (done) => {
-        server.inject({ method: 'post', url: '/pokemons', payload: { name: 'Foobar' } }, () => {
-          server.inject('/pokemons', (res) => {
-            const foobar = res.result.find(pokemon => pokemon.name === 'Foobar');
-            expect(foobar).to.exist;
+        it('should return 201', (done) => {
+          server.inject({ method : 'post', url : '/pokemons?api_key=anass', payload : { name : 'Foobar' } }, (res) => {
+            expect(res.statusCode).to.equal(201);
             done();
+          });
+        });
+        it('should add a new pokemon to the list', (done) => {
+          server.inject({ method : 'post', url : '/pokemons?api_key=anass', payload : { name : 'Foobar' } }, () => {
+            server.inject('/pokemons?api_key=anass', (res) => {
+              const foobar = res.result.find(pokemon => pokemon.name === 'Foobar');
+              expect(foobar).to.exist;
+              done();
+            });
           });
         });
       });
